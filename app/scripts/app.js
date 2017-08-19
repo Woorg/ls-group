978d015a00b364a3fe82883eee4221fc97717ebf
Igor Gorlov <gorlov35@gmail.com>
2017-08-19 17:38:48 +0300

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+import svg4everybody from 'svg4everybody';
+import $ from 'jquery';
+// import 'slick-carousel';
+
+
+(function ($) {
+
+	$(function () {
+		svg4everybody();
+
+
+	});
+
+
+})(jQuery);
+
+
+
